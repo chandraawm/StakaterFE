bb4864ac5f72a667de66365eb993105bee229bfb
chthanip <chandrareddy.thaniparthy@dhl.com>
2021-06-12 12:42:29 +0800

    FE service changes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# StakaterFE
+# StakaterFe Service Notes
+
+This FrondEnd Service Calls backend StakaterBE service and prints the string returned from backend and
+appends the date and time. 
+
+Sample Output : 12/06/2021 12:02:36 Hello Chandra Reddy
+
+
+
+Technologies Used : Java , Spring Boot web application with jsp , RestTemplate , Embeded Tomcat
+Deployment : openshift used
+
+Deployment Notes : 
+
+1. Port Used : 8080 . configured in application.properties
+
+2. Sample url : http://localhost:8080/ ("/" is the Rest URI path)
+
+3. Sample OUTPUT of the service is ::  12/06/2021 12:02:36 Hello Chandra Reddy (Hello Chandra Reddy is the output received from backend service)
+
+4. While executing the FE service , BE service should be up and running state to display proper output.
---
+++ src/main/java/com/mkyong/WelcomeController.java
@@ -0,0 +1,35 @@
+package com.mkyong;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class WelcomeController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+	
+	
+
+	@RequestMapping("/")
+	public String displayHello(Map<String, Object> model) {
+	    
+	    LocalDateTime now = LocalDateTime.now();
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        String formatDateTime = now.format(formatter);
+	    
+	    RestTemplate rt = new RestTemplate();
+	    String res= rt.getForObject("http://localhost:8081/displayHello", String.class);
+		model.put("message", formatDateTime +" "+ res);
+		return "welcome";
+	}
+
+}
